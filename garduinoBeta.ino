53aa07d7d0ccc5443976bc57aadbd879354780b0
jaoroca <jaoroca@gmail.com>
2017-07-09 18:41:47 -0600

    Add files via upload

---
+++ garduinoBeta.ino
@@ -0,0 +1,20 @@
+int waterPump = 13;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(waterPump, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int humidityRaw = analogRead(A0);
+  int humidityReal = map(humidityRaw, 1023, 0, 0, 100);
+  Serial.println(humidityReal);
+  delay(100);
+  if (humidityReal < 60)
+  {
+    digitalWrite(waterPump, LOW);
+  }else{
+    digitalWrite(waterPump, HIGH);
+  }
+}
